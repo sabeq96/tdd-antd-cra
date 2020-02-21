fce206d450a91f0e6aab6fc7668809e0ceb8a30d
sabek <sdawid96@gmail.com>
2020-02-21 22:58:29 +0100

    commit build

---
+++ build/precache-manifest.f03509febd9eb99dbf79099d2b55d4a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad1cf685d2e9b922208e0780fdf8714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c41d1334f823feb5388",
+    "url": "/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "6c41d1334f823feb5388",
+    "url": "/static/js/2.9727d326.chunk.js"
+  },
+  {
+    "revision": "9211ad15d50dac51bfb2123027018d91",
+    "url": "/static/js/2.9727d326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22e03188c90f938bfad",
+    "url": "/static/js/main.1985f069.chunk.js"
+  },
+  {
+    "revision": "3789966722f6f2e24ec7",
+    "url": "/static/js/runtime-main.e4f9e57e.js"
+  }
+]);
